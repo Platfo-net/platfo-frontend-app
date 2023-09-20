5bf389f2f40032c94d838d9818e8c93a7837b357
Hamid Samimi <samimi.electronics@gmail.com>
2023-09-20 23:46:15 +0330

    [feature] refactor store page to contain store list

--- src/pages/dashboard/store/StoreListPage.vue
+++ src/pages/dashboard/store/StoreListPage.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import StoreList from 'components/dashboard/store-page/StoreList.vue';
+</script>
 
 <template>
-  <q-page class="q-ma-md"> Store Page List </q-page>
+  <q-page class="q-pa-md">
+    <div class="row justify-between items-center q-mb-lg">
+      <div class="text-h5">My Stores</div>
+      <q-btn
+        color="accent"
+        :to="{ name: 'CreateStorePage' }"
+        label="Create new store"
+      ></q-btn>
+    </div>
+    <StoreList />
+  </q-page>
 </template>
 
 <style scoped></style>
