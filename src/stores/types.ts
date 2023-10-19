533e5612323e4b2256a68fd5ecccb89a0c020619
Hamid <samimi.electronics@gmail.com>
2023-10-20 02:49:25 +0330

    [feature] created shopping cart models

--- src/stores/types.ts
+++ src/stores/types.ts
@@ -1,3 +1,5 @@
+import { IProduct } from 'components/models';
+
 export type AuthState = {
   access_token: string;
   token_type: string;
@@ -7,4 +9,13 @@ export type AuthState = {
 export interface ILoginApiResponse {
   access_token: string;
   token_type: string;
+}
+
+export interface IShoppingCartItem {
+  product_id: string;
+  count: number;
+}
+
+export interface IShoppingCart {
+  items: Record<string, IShoppingCartItem>;
 }
