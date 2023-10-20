f1350201acf1aaa2b19ce6860253028709cb0ad1
Hamid <samimi.electronics@gmail.com>
2023-10-20 16:49:53 +0330

    [feature] show shopping cart items in shopping cart page

--- src/pages/public/shop/ShopShoppingCartPage.vue
+++ src/pages/public/shop/ShopShoppingCartPage.vue
@@ -1,8 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useShoppingCart } from 'stores/shopping-cart-store';
+import TelegramShopCartItem from 'components/public/shop/TelegramShopCartItem.vue';
+
+const shoppingCart = useShoppingCart();
+</script>
 
 <template>
   <q-page class="q-pa-md">
-    <div class="text-h4">سبد خرید</div>
+    <div class="text-h4 q-my-lg">سبد خرید</div>
+    <template v-if="!Object.keys(shoppingCart.get().items).length">
+      <p>شما محصولی را به سبد خرید خود اضافه نکردید</p>
+    </template>
+    <template v-else>
+      <div
+        v-for="shoppingCartItem in shoppingCart.get().items"
+        :key="shoppingCartItem.product.id"
+      >
+        <telegram-shop-cart-item :product="shoppingCartItem.product" />
+      </div>
+    </template>
   </q-page>
 </template>
 
