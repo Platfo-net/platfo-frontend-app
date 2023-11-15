df5cb796dfc5279f5ba0de5715bb421c3cca51df
Hamid <samimi.electronics@gmail.com>
2023-11-15 21:55:23 +0330

    [feature] created base loading spinner

---
+++ src/components/common/BaseLoadingSpinner.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  loading: boolean;
+}>();
+</script>
+
+<template>
+  <q-inner-loading :showing="loading">
+    <q-spinner></q-spinner>
+    <div class="q-my-md">
+      {{ $t('general.gettingData') }}
+    </div>
+  </q-inner-loading>
+</template>
+
+<style scoped></style>
