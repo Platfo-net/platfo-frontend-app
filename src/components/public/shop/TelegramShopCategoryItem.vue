3cdd6d73b044ca2ef516c0f78516d382588fd65e
Hamid Samimi <samimi.electronics@gmail.com>
2023-12-09 01:10:56 +0330

    [feature] created telegram shop category item component

---
+++ src/components/public/shop/TelegramShopCategoryItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <q-card class="shadow-4" style="border-radius: 7px;">
+        <q-img style="overflow: hidden;" src="https://placehold.co/100x100" fit="cover" />
+        <q-card-section class="text-center text-bold bg-orange text-white">
+            <div>{{ title }}</div>
+        </q-card-section>
+    </q-card>
+</template>
+
+<script setup lang="ts">
+import { IProductCategory } from 'src/components/models';
+
+defineProps<IProductCategory>();
+</script>
+
+<style scoped>
+
+</style>
