032b2170503938b5089cffe03f41ec990243035b
Hamid Samimi <samimi.electronics@gmail.com>
2023-12-11 00:04:48 +0330

    [feature] route to ShopCategoryProductsPage on click

--- src/components/public/shop/TelegramShopCategoryItem.vue
+++ src/components/public/shop/TelegramShopCategoryItem.vue
@@ -1,7 +1,7 @@
 <template>
-    <q-card class="shadow-4" style="border-radius: 7px;">
-        <q-img style="overflow: hidden;" src="https://placehold.co/100x100" fit="cover" />
-        <q-card-section class="text-center text-bold bg-orange text-white">
+    <q-card class="q-hoverable shadow-4 cursor-pointer" v-ripple style="border-radius: 7px; border-bottom: orange 5px solid;" @click="goToShopCategoryProductsPage(id)">
+        <!-- <q-img style="overflow: hidden;" src="https://placehold.co/100x100" fit="cover" /> -->
+        <q-card-section class="text-center text-bold">
             <div>{{ title }}</div>
         </q-card-section>
     </q-card>
@@ -9,8 +9,15 @@
 
 <script setup lang="ts">
 import { IProductCategory } from 'src/components/models';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+function goToShopCategoryProductsPage(categoryId: string) {
+    router.push({ name: 'ShopCategoryProductsPage', params: {
+        categoryId,
+    }, query: { categoryTitle: props.title }});
+}
 
-defineProps<IProductCategory>();
+const props = defineProps<IProductCategory>();
 </script>
 
 <style scoped>
