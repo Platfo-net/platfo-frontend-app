58eddf5a923abbecbc665250612ed96d3d3fbd5c
Hamid Samimi <samimi.electronics@gmail.com>
2023-12-11 00:02:25 +0330

    [feature] service for getting shop category products

--- src/services/useTelegramShopService.ts
+++ src/services/useTelegramShopService.ts
@@ -1,16 +1,21 @@
 import { useApi } from '../composables/use-api';
 
-const { api, loading } = useApi();
+const shopApi = useApi();
 
 export const useTelegramShopService = () => {
     const getShopCategories = async (shopId: string) => {
-        const response = await api.get(`/shop/categories/telegram/${shopId}/all`);
+        const response = await shopApi.api.get(`/shop/categories/telegram/${shopId}/all`);
+        return response.data;
+    }
+    const getShopCategoryProducts = async (shopId: string, categoryId: string) => {
+        const response = await shopApi.api.get(`/shop/products/telegram/${shopId}/all?category_id=${categoryId}`);
         return response.data;
     }
     return {
-        loading,
+        loading: shopApi.loading,
         queries: {
             getShopCategories,
+            getShopCategoryProducts,
         },
     }
 }
