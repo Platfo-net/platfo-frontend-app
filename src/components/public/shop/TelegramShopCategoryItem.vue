f1aca218534f08b714b4a2a29968d95036d19617
Hamid <samimi.electronics@gmail.com>
2023-12-30 22:39:29 +0330

    [feature] Show image categories

--- src/components/public/shop/TelegramShopCategoryItem.vue
+++ src/components/public/shop/TelegramShopCategoryItem.vue
@@ -1,10 +1,15 @@
 <template>
-    <q-card class="q-hoverable shadow-4 cursor-pointer" v-ripple style="border-radius: 7px; border-bottom: orange 5px solid;" @click="goToShopCategoryProductsPage(id)">
-        <!-- <q-img style="overflow: hidden;" src="https://placehold.co/100x100" fit="cover" /> -->
-        <q-card-section class="text-center text-bold">
-            <div>{{ title }}</div>
-        </q-card-section>
-    </q-card>
+  <q-card
+    class="q-hoverable shadow-4 cursor-pointer"
+    v-ripple
+    style="border-radius: 7px; border-bottom: orange 5px solid"
+    @click="goToShopCategoryProductsPage(id)"
+  >
+    <q-img style="overflow: hidden" :src="getCategoryImageSrc()" fit="cover" />
+    <q-card-section class="text-center text-bold">
+      <div>{{ title }}</div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup lang="ts">
@@ -12,14 +17,18 @@ import { IProductCategory } from 'src/components/models';
 import { useRouter } from 'vue-router';
 const router = useRouter();
 function goToShopCategoryProductsPage(categoryId: string) {
-    router.push({ name: 'ShopCategoryProductsPage', params: {
-        categoryId,
-    }, query: { categoryTitle: props.title }});
+  router.push({
+    name: 'ShopCategoryProductsPage',
+    params: {
+      categoryId,
+    },
+    query: { categoryTitle: props.title },
+  });
 }
-
+const getCategoryImageSrc = () => {
+  return props.image_url || 'https://placehold.co/100x100';
+};
 const props = defineProps<IProductCategory>();
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
