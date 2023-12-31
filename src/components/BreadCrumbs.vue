20cfbd2f8ed51f8e309beb3409906d86e0a9c8e6
Hamid Samimi <samimi.electronics@gmail.com>
2023-12-31 15:46:37 +0330

    code format

--- src/components/BreadCrumbs.vue
+++ src/components/BreadCrumbs.vue
@@ -6,12 +6,7 @@ const route = useRoute();
 <template>
   <template v-if="route.meta.breadcrumbs">
     <q-breadcrumbs>
-      <q-breadcrumbs-el
-        v-for="bc in route.meta.breadcrumbs"
-        :label="$t(bc.label)"
-        :to="bc.to"
-        :key="bc.label"
-      />
+      <q-breadcrumbs-el v-for="bc in route.meta.breadcrumbs" :label="$t(bc.label)" :to="bc.to" :key="bc.label" />
     </q-breadcrumbs>
   </template>
 </template>
