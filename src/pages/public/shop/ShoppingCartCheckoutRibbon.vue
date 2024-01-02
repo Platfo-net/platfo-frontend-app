8e020d3960c6cd967d07502b895192425e609412
Hamid <samimi.electronics@gmail.com>
2024-01-02 23:11:05 +0330

    [bugfix] ribbon not sliding away

--- src/pages/public/shop/ShoppingCartCheckoutRibbon.vue
+++ src/pages/public/shop/ShoppingCartCheckoutRibbon.vue
@@ -48,7 +48,11 @@ const dataToSend = reactive<ICreateShopOrderBody>({
   shipment_method_id: '',
 });
 
-const enableCheckout = computed(() => dataToSend.shipment_method_id?.length || !dataToSend.payment_method_id?.length);
+const enableCheckout = computed(
+  () =>
+    dataToSend.shipment_method_id?.length ||
+    !dataToSend.payment_method_id?.length
+);
 
 const checkout = async () => {
   if (!shopPaymentMethods.value) {
@@ -93,148 +97,244 @@ onMounted(async () => {
 </script>
 
 <template>
-  <q-dialog v-model="showCheckoutDialog" persistent maximized transition-show="slide-up" transition-hide="slide-down">
-    <q-card class="bg-white">
-      <q-toolbar class="q-py-md bg-white shadow-10">
-        <q-toolbar-title>تایید نهایی و پرداخت</q-toolbar-title>
-        <q-btn round flat icon="close" @click="showCheckoutDialog = false"></q-btn>
-        <div style="font-size: 1.5rem" class="q-ml-lg text-bold"></div>
-      </q-toolbar>
-      <q-space></q-space>
-      <q-card-section>
-        <div class="text-body1">اطلاعات شخصی</div>
-        <q-input class="q-my-md" dense outlined color="orange" v-model="dataToSend.first_name" name="first_name"
-          label="نام"></q-input>
-        <q-input class="q-my-md" dense outlined color="orange" v-model="dataToSend.last_name" name="last_name"
-          label="نام خانوادگی"></q-input>
-        <q-input dir="ltr" class="q-my-md" dense outlined color="orange" v-model="dataToSend.phone_number"
-          name="phone_number" label="شماره تماس"></q-input>
-        <q-input class="q-my-md" dense outlined type="textarea" color="orange" v-model="dataToSend.address" name="address"
-          label="آدرس محل سکونت"></q-input>
-      </q-card-section>
-      <q-card-section>
-        <div class="text-body1">روش ارسال</div>
-        <template v-if="!shopShippingMethods?.length">
-          روش ارسالی ثبت نشده است
-        </template>
-        <template v-else>
-          <q-list>
-            <q-item v-ripple class="q-my-md rounded-borders" style="border: 1px solid #e1e1e1"
-              v-for="shippingMethod in shopShippingMethods" :key="shippingMethod.id" tag="label">
-              <q-item-section avatar>
-                <q-radio v-model="dataToSend.shipment_method_id" :val="shippingMethod.id" color="orange" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>{{ shippingMethod.title }}</q-item-label>
-                <q-item-label caption>هزینه ارسال:
-                  <span v-if="shippingMethod.price != 0">
-                    {{
-                      Intl.NumberFormat('fa', {
-                        currency: 'IRT',
-                      }).format(shippingMethod.price as number)
-                    }} <toman-symbol :size="12"></toman-symbol>
-                  </span>
-                  <span v-else>رایگان</span>
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </template>
-      </q-card-section>
-      <q-card-section>
-        <div class="text-body1">روش پرداخت</div>
-        <template v-for="paymentMethod in shopPaymentMethods" :key="paymentMethod.id">
-          <template v-if="paymentMethod.items.length">
-            <template v-for="innerPm in paymentMethod.items" :key="innerPm.id">
-              <div class="q-ml-lg q-my-md">پراخت آنلاین</div>
+  <div
+    class="flex row q-pa-md q-px-lg items-center justify-between blur"
+    style="border-bottom: 1px solid #e2e2e2"
+  >
+    <q-dialog
+      v-model="showCheckoutDialog"
+      persistent
+      maximized
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card class="bg-white">
+        <q-toolbar class="q-py-md bg-white shadow-10">
+          <q-toolbar-title>تایید نهایی و پرداخت</q-toolbar-title>
+          <q-btn
+            round
+            flat
+            icon="close"
+            @click="showCheckoutDialog = false"
+          ></q-btn>
+          <div style="font-size: 1.5rem" class="q-ml-lg text-bold"></div>
+        </q-toolbar>
+        <q-space></q-space>
+        <q-card-section>
+          <div class="text-body1">اطلاعات شخصی</div>
+          <q-input
+            class="q-my-md"
+            dense
+            outlined
+            color="orange"
+            v-model="dataToSend.first_name"
+            name="first_name"
+            label="نام"
+          ></q-input>
+          <q-input
+            class="q-my-md"
+            dense
+            outlined
+            color="orange"
+            v-model="dataToSend.last_name"
+            name="last_name"
+            label="نام خانوادگی"
+          ></q-input>
+          <q-input
+            dir="ltr"
+            class="q-my-md"
+            dense
+            outlined
+            color="orange"
+            v-model="dataToSend.phone_number"
+            name="phone_number"
+            label="شماره تماس"
+          ></q-input>
+          <q-input
+            class="q-my-md"
+            dense
+            outlined
+            type="textarea"
+            color="orange"
+            v-model="dataToSend.address"
+            name="address"
+            label="آدرس محل سکونت"
+          ></q-input>
+        </q-card-section>
+        <q-card-section>
+          <div class="text-body1">روش ارسال</div>
+          <template v-if="!shopShippingMethods?.length">
+            روش ارسالی ثبت نشده است
+          </template>
+          <template v-else>
+            <q-list>
+              <q-item
+                v-ripple
+                class="q-my-md rounded-borders"
+                style="border: 1px solid #e1e1e1"
+                v-for="shippingMethod in shopShippingMethods"
+                :key="shippingMethod.id"
+                tag="label"
+              >
+                <q-item-section avatar>
+                  <q-radio
+                    v-model="dataToSend.shipment_method_id"
+                    :val="shippingMethod.id"
+                    color="orange"
+                  />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>{{ shippingMethod.title }}</q-item-label>
+                  <q-item-label caption
+                    >هزینه ارسال:
+                    <span v-if="shippingMethod.price != 0">
+                      {{
+                        Intl.NumberFormat('fa', {
+                          currency: 'IRT',
+                        }).format(shippingMethod.price as number)
+                      }}
+                      <toman-symbol :size="12"></toman-symbol>
+                    </span>
+                    <span v-else>رایگان</span>
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </template>
+        </q-card-section>
+        <q-card-section>
+          <div class="text-body1">روش پرداخت</div>
+          <template
+            v-for="paymentMethod in shopPaymentMethods"
+            :key="paymentMethod.id"
+          >
+            <template v-if="paymentMethod.items.length">
+              <template
+                v-for="innerPm in paymentMethod.items"
+                :key="innerPm.id"
+              >
+                <div class="q-ml-lg q-my-md">پراخت آنلاین</div>
+                <q-list>
+                  <q-item
+                    class="q-my-md rounded-borders"
+                    style="border: 1px solid #e1e1e1"
+                    tag="label"
+                  >
+                    <q-item-section avatar>
+                      <q-radio
+                        v-model="dataToSend.payment_method_id"
+                        :val="innerPm.id"
+                        color="orange"
+                      />
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>{{ innerPm.title }}</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </template>
+            </template>
+            <template v-else>
+              <div class="q-ml-lg q-my-md">پرداخت نقدی</div>
               <q-list>
-                <q-item class="q-my-md rounded-borders" style="border: 1px solid #e1e1e1" tag="label">
+                <q-item
+                  class="q-my-md rounded-borders"
+                  style="border: 1px solid #e1e1e1"
+                  tag="label"
+                >
                   <q-item-section avatar>
-                    <q-radio v-model="dataToSend.payment_method_id" :val="innerPm.id" color="orange" />
+                    <q-radio
+                      v-model="dataToSend.payment_method_id"
+                      :val="paymentMethod.id"
+                      color="orange"
+                    />
                   </q-item-section>
                   <q-item-section>
-                    <q-item-label>{{ innerPm.title }}</q-item-label>
+                    <q-item-label>{{ paymentMethod.title }}</q-item-label>
                   </q-item-section>
                 </q-item>
               </q-list>
             </template>
           </template>
-          <template v-else>
-            <div class="q-ml-lg q-my-md">پرداخت نقدی</div>
-            <q-list>
-              <q-item class="q-my-md rounded-borders" style="border: 1px solid #e1e1e1" tag="label">
-                <q-item-section avatar>
-                  <q-radio v-model="dataToSend.payment_method_id" :val="paymentMethod.id" color="orange" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label>{{ paymentMethod.title }}</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </template>
-        </template>
-      </q-card-section>
-      <q-card-section>
-        <div class="flex row q-pa-md q-px-lg items-center justify-between blur">
-          <div class="flex column text-dark">
-            <div class="text-bold">{{ $t('general.payable') }}</div>
-            <div>
-              {{
-                Intl.NumberFormat('fa', {
-                  currency: 'IRT',
-                }).format(shoppingCart.totalCartAmount() + (shopShippingMethods?.find(x => x.id ===
-                  dataToSend.shipment_method_id)?.price as number))
-              }}
-              <toman-symbol :size="16"></toman-symbol>
+        </q-card-section>
+        <q-card-section>
+          <div
+            class="flex row q-pa-md q-px-lg items-center justify-between blur"
+          >
+            <div class="flex column text-dark">
+              <div class="text-bold">{{ $t('general.payable') }}</div>
+              <div>
+                {{
+                  Intl.NumberFormat('fa', {
+                    currency: 'IRT',
+                  }).format(
+                    shoppingCart.totalCartAmount() +
+                      (shopShippingMethods?.find(
+                        (x) => x.id === dataToSend.shipment_method_id
+                      )?.price as number)
+                  )
+                }}
+                <toman-symbol :size="16"></toman-symbol>
+              </div>
             </div>
+            <!-- <q-btn @click="showCheckoutDialog = true" label="مرحله&zwnj;ی بعد" color="orange" /> -->
+            <q-btn
+              @click="showConfirmDialog = true"
+              color="orange"
+              :disable="!enableCheckout"
+              >پرداخت سفارش</q-btn
+            >
           </div>
-          <!-- <q-btn @click="showCheckoutDialog = true" label="مرحله&zwnj;ی بعد" color="orange" /> -->
-          <q-btn @click="showConfirmDialog = true" color="orange" :disable="!enableCheckout">پرداخت
-            سفارش</q-btn>
-        </div>
-      </q-card-section>
-    </q-card>
-  </q-dialog>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
 
-  <q-dialog class="font-vazir" v-model="showConfirmDialog">
-    <q-card>
-      <q-card-section dir="rtl">
-        <div class="text-negative text-bold">هشدار</div>
-      </q-card-section>
-      <q-separator />
-      <q-card-section dir="rtl">
-        <p>
-          آیا از محصولات انتخاب شده در سبد خرید و اطلاعات ثبت شده اطمینان
-          دارید؟
-        </p>
-      </q-card-section>
-      <q-card-actions>
-        <q-btn size="sm" color="primary" :loading="loading" @click="checkout">بله</q-btn>
-        <q-btn size="sm" color="negative" :disable="loading" @click="showConfirmDialog = false">خیر</q-btn>
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
+    <q-dialog class="font-vazir" v-model="showConfirmDialog">
+      <q-card>
+        <q-card-section dir="rtl">
+          <div class="text-negative text-bold">هشدار</div>
+        </q-card-section>
+        <q-separator />
+        <q-card-section dir="rtl">
+          <p>
+            آیا از محصولات انتخاب شده در سبد خرید و اطلاعات ثبت شده اطمینان
+            دارید؟
+          </p>
+        </q-card-section>
+        <q-card-actions>
+          <q-btn size="sm" color="primary" :loading="loading" @click="checkout"
+            >بله</q-btn
+          >
+          <q-btn
+            size="sm"
+            color="negative"
+            :disable="loading"
+            @click="showConfirmDialog = false"
+            >خیر</q-btn
+          >
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 
-  <q-dialog class="font-vazir" v-model="showCheckoutSuccessDialog">
-    <q-card>
-      <q-card-section dir="rtl">
-        <div class="text-primary text-bold">ثبت موفق</div>
-      </q-card-section>
-      <q-separator />
-      <q-card-section dir="rtl">
-        <p>شماره سفارش شما ثبت شد:</p>
-        <p class="text-bold">{{ createdOrderId }}</p>
-        <p>
-          لطفا برای مشاهده سفارش و پرداخت آن، گزینه «بازگشت به بات» را انتخاب
-          کنید.
-        </p>
-      </q-card-section>
-      <q-card-actions align="center">
-        <q-btn size="sm" color="orange" @click="cleanup">بازگشت به بات</q-btn>
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
-  <div class="flex row q-pa-md q-px-lg items-center justify-between blur" style="border-bottom: 1px solid #e2e2e2">
+    <q-dialog class="font-vazir" v-model="showCheckoutSuccessDialog">
+      <q-card>
+        <q-card-section dir="rtl">
+          <div class="text-primary text-bold">ثبت موفق</div>
+        </q-card-section>
+        <q-separator />
+        <q-card-section dir="rtl">
+          <p>شماره سفارش شما ثبت شد:</p>
+          <p class="text-bold">{{ createdOrderId }}</p>
+          <p>
+            لطفا برای مشاهده سفارش و پرداخت آن، گزینه «بازگشت به بات» را انتخاب
+            کنید.
+          </p>
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn size="sm" color="orange" @click="cleanup">بازگشت به بات</q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
     <div class="flex column text-dark">
       <div class="text-bold">مجموع سفارش</div>
       <div>
@@ -246,7 +346,11 @@ onMounted(async () => {
         <toman-symbol :size="16"></toman-symbol>
       </div>
     </div>
-    <q-btn @click="showCheckoutDialog = true" label="مرحله&zwnj;ی بعد" color="orange" />
+    <q-btn
+      @click="showCheckoutDialog = true"
+      label="مرحله&zwnj;ی بعد"
+      color="orange"
+    />
   </div>
 </template>
 
