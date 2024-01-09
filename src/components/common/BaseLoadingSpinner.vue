b58e7ec905f493736a7030bed0fa46f694dde6e8
Hamid <samimi.electronics@gmail.com>
2024-01-09 22:41:22 +0330

    [fix] show on top of other components

--- src/components/common/BaseLoadingSpinner.vue
+++ src/components/common/BaseLoadingSpinner.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <q-inner-loading :showing="loading">
+  <q-inner-loading style="z-index: 3" :showing="loading">
     <div class="flex column items-center justify-center">
       <q-spinner></q-spinner>
       <div class="q-my-md">
