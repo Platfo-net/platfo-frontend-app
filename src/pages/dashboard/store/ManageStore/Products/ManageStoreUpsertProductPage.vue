25192ca61ef63447c846dbe1ea69fb4dd64ca6e4
Hamid <samimi.electronics@gmail.com>
2024-01-09 22:39:10 +0330

    [feature] rename page

---
+++ src/pages/dashboard/store/ManageStore/Products/ManageStoreUpsertProductPage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import UpsertProductForm from 'components/dashboard/store-page/UpsertProductForm.vue';
+
+/** COMPONENT COMPOSABLES */
+
+/************************ */
+
+/** COMPONENT STATE */
+
+/****************** */
+
+/** COMPONENT DEFINES */
+
+/******************** */
+
+/** COMPONENT FUNCTIONS */
+
+/********************** */
+
+/** COMPONENT LIFECYCLE HANDLERS */
+
+/******************************* */
+</script>
+
+<template>
+  <upsert-product-form />
+</template>
+
+<style scoped></style>
