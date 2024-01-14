917a2583b42ae8f58ef6fccb26e5ac4963371aaf
Hamid Samimi <samimi.electronics@gmail.com>
2024-01-14 13:34:00 +0330

    [feature] refactor sidebar user information to component

---
+++ src/components/common/BaseLayoutSidebarUserInfo.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useAuthStore } from 'src/stores/auth-store';
+import { useRouter } from 'vue-router';
+
+/** COMPONENT COMPOSABLES */
+const authStore = useAuthStore();
+const router = useRouter();
+/************************ */
+
+/** COMPONENT STATE */
+
+/****************** */
+
+/** COMPONENT DEFINES */
+
+/******************** */
+
+/** COMPONENT FUNCTIONS */
+const handleLogoutClick = () => {
+  authStore.actions.clearAuthState();
+  router.replace({
+    name: 'LoginPage',
+  });
+};
+/********************** */
+
+/** COMPONENT LIFECYCLE HANDLERS */
+
+/******************************* */
+</script>
+
+<template>
+  <q-img class="absolute-top bg-primary" style="height: 161.6px">
+    <div class="absolute-bottom bg-transparent">
+      <q-avatar size="56px" class="q-mb-sm">
+        <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+      </q-avatar>
+      <div class="flex row items-center justify-between">
+        <div>
+          <div class="text-weight-bold">جعفر لولویی</div>
+          <div>09123456789</div>
+        </div>
+
+        <q-btn class="q-ma-md" round text-color="white" icon="logout" flat @click="handleLogoutClick" dense>
+          <q-tooltip>خروج</q-tooltip></q-btn>
+      </div>
+    </div>
+  </q-img>
+</template>
+
+<style scoped></style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,51 +2,14 @@
   <q-layout view="lHh Lpr lFf">
     <q-header bordered>
       <q-toolbar class="mobile-only">
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
         <q-toolbar-title> {{ $t('layout.appHeader') }} </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      style="border: 1px solid #e2e2e2"
-    >
-      <base-layout-sidebar-menu
-        :routes="mainLayoutRoutes"
-      ></base-layout-sidebar-menu>
-      <q-img class="absolute-top bg-primary" style="height: 161.6px">
-        <div class="absolute-bottom bg-transparent">
-          <q-avatar size="56px" class="q-mb-sm">
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-          </q-avatar>
-          <div class="flex row items-center justify-between">
-            <div>
-              <div class="text-weight-bold">جعفر لولویی</div>
-              <div>09123456789</div>
-            </div>
-
-            <q-btn
-              class="q-ma-md"
-              round
-              text-color="white"
-              icon="logout"
-              flat
-              @click="handleLogoutClick"
-              dense
-            >
-              <q-tooltip>خروج</q-tooltip></q-btn
-            >
-          </div>
-        </div>
-      </q-img>
+    <q-drawer v-model="leftDrawerOpen" show-if-above style="border: 1px solid #e2e2e2">
+      <base-layout-sidebar-menu :routes="mainLayoutRoutes"></base-layout-sidebar-menu>
+      <base-layout-sidebar-user-info></base-layout-sidebar-user-info>
     </q-drawer>
 
     <q-page-container class="bg-teal-1">
@@ -57,20 +20,9 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useAuthStore } from 'stores/auth-store';
-import { useRouter } from 'vue-router';
 import { useSidebar } from 'src/composables/use-sidebar';
 import BaseLayoutSidebarMenu from 'components/common/BaseLayoutSidebarMenu.vue';
-
-const authStore = useAuthStore();
-const router = useRouter();
-
-const handleLogoutClick = () => {
-  authStore.actions.clearAuthState();
-  router.replace({
-    name: 'LoginPage',
-  });
-};
+import BaseLayoutSidebarUserInfo from 'src/components/common/BaseLayoutSidebarUserInfo.vue';
 
 const { mainLayoutRoutes } = useSidebar();
 
