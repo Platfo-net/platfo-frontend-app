a680194b7c30fda9ffb4e026bb366ed7613f54a9
Hamid <samimi.electronics@gmail.com>
2024-04-20 00:28:04 +0330

    [feature] initial implementation of chatbot basic information page

---
+++ src/pages/dashboard/chatbot/ManageChatBot/ManageChatbotBasicInformationPage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+/** COMPONENT COMPOSABLES */
+
+/************************ */
+
+/** COMPONENT STATE */
+
+/****************** */
+
+/** COMPONENT DEFINES */
+
+/******************** */
+
+/** COMPONENT FUNCTIONS */
+
+/********************** */
+
+/** COMPONENT LIFECYCLE HANDLERS */
+
+/******************************* */
+</script>
+
+<template>
+  <q-page class="q-pa-md container">
+    Basic information
+  </q-page>
+</template>
+
+<style scoped></style>
