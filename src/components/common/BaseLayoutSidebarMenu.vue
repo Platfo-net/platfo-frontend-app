506bea717cdf47e531fb4af7925046d3eaa52067
Hamid <samimi.electronics@gmail.com>
2024-04-29 00:31:46 +0330

    [feature] styling changes

--- src/components/common/BaseLayoutSidebarMenu.vue
+++ src/components/common/BaseLayoutSidebarMenu.vue
@@ -32,14 +32,8 @@ defineProps<BaseLayoutSidebarMenuProps>();
   <q-scroll-area style="height: calc(100% - 161.6px); margin-top: 161.6px">
     <q-list padding>
       <div v-for="(route, idx) in routes" :key="route.title + idx">
-        <q-item
-          :active-class="
-            route.activeClass?.length ? route.activeClass : 'bg-teal-1'
-          "
-          clickable
-          v-if="!route.children?.length"
-          :to="route.to"
-        >
+        <q-item :active-class="route.activeClass?.length ? route.activeClass : 'bg-grey-3'
+        " clickable v-if="!route.children?.length" :to="route.to">
           <q-item-section avatar>
             <q-icon :name="route.icon"></q-icon>
           </q-item-section>
@@ -48,22 +42,12 @@ defineProps<BaseLayoutSidebarMenuProps>();
           </q-item-section>
         </q-item>
         <q-expansion-item v-else :label="route.title" :icon="route.icon">
-          <div
-            v-for="(innerRoute, innerIdx) in route.children"
-            :key="innerRoute.title + innerIdx"
-          >
-            <q-item
-              :active-class="
-                innerRoute.activeClass?.length
-                  ? innerRoute.activeClass
-                  : 'bg-teal-1'
-              "
-              v-if="!innerRoute.children?.length"
-              :key="innerRoute.title + innerIdx"
-              class="q-pl-xl"
-              clickable
-              :to="innerRoute.to"
-            >
+          <div v-for="(innerRoute, innerIdx) in route.children" :key="innerRoute.title + innerIdx">
+            <q-item :active-class="innerRoute.activeClass?.length
+        ? innerRoute.activeClass
+        : 'bg-grey-3'
+        " v-if="!innerRoute.children?.length" :key="innerRoute.title + innerIdx" class="q-pl-xl" clickable
+              :to="innerRoute.to">
               <q-item-section avatar>
                 <q-icon :name="innerRoute.icon"></q-icon>
               </q-item-section>
@@ -71,21 +55,11 @@ defineProps<BaseLayoutSidebarMenuProps>();
                 {{ innerRoute.title }}
               </q-item-section>
             </q-item>
-            <q-expansion-item
-              header-class="q-pl-xl"
-              v-else
-              :label="innerRoute.title"
-              :icon="innerRoute.icon"
-            >
-              <q-item
-                v-for="(
+            <q-expansion-item header-class="q-pl-xl" v-else :label="innerRoute.title" :icon="innerRoute.icon">
+              <q-item v-for="(
                   innerInnerRoute, innerInnerRouteIdx
-                ) in innerRoute.children"
-                :key="innerInnerRoute.title + innerInnerRouteIdx"
-                class="pl-2xl"
-                clickable
-                :to="innerInnerRoute.to"
-              >
+                ) in innerRoute.children" :key="innerInnerRoute.title + innerInnerRouteIdx" class="pl-2xl" clickable
+                :to="innerInnerRoute.to">
                 <q-item-section avatar>
                   <q-icon :name="innerInnerRoute.icon"></q-icon>
                 </q-item-section>
--- src/pages/public/RegisterPage.vue
+++ src/pages/public/RegisterPage.vue
@@ -74,97 +74,65 @@ const handleFormSubmit = () => {
 
 <template>
   <q-page class="flex justify-center items-center bg-grey-3 q-pa-lg">
-    <q-card
-      flat
-      style="min-width: 300px; width: 100%; max-width: 995px; border-radius: 35px;"
-      class="flex row"
-    >
+    <q-card flat style="min-width: 300px; width: 100%; max-width: 995px; border-radius: 35px;" class="flex row">
       <q-card-section class="col-md-6 col-12">
         <div class="q-pa-lg">
 
-          <div class="text-h6 q-mb-lg text-bold">{{$t('pages.public.register.title')}}</div>
+          <div class="text-h6 q-mb-lg text-bold">{{ $t('pages.public.register.title') }}</div>
           <form class="q-pa-md" @submit.prevent="handleFormSubmit">
             <div class="q-gutter-md">
               <div class="row q-col-gutter-md">
                 <div class="col-md-6 col-12">
                   <div class="q-mb-sm">{{ $t('pages.public.register.fields.firstname') }}</div>
-                  <q-input
-                    rounded outlined
-                    name="first_name"
-                    v-model="authStore.registerState.first_name"
-                    type="text"
-                    color="primary"
-                    :rules="[(val) => !!val || $t('general.fields.requiredStringField')]"
-                  />
+                  <q-input rounded outlined name="first_name" v-model="authStore.registerState.first_name" type="text"
+                    color="primary" :rules="[(val) => !!val || $t('general.fields.requiredStringField')]" />
                 </div>
                 <div class="col-md-6 col-12">
                   <div class="q-mb-sm">{{ $t('pages.public.register.fields.lastname') }}</div>
-                  <q-input
-                    rounded outlined
-                    color="primary"
-                    name="last_name"
-                    v-model="authStore.registerState.last_name"
-                    type="text"
-                    :rules="[(val) => !!val || $t('general.fields.requiredStringField')]"
-                  />
+                  <q-input rounded outlined color="primary" name="last_name" v-model="authStore.registerState.last_name"
+                    type="text" :rules="[(val) => !!val || $t('general.fields.requiredStringField')]" />
                 </div>
                 <div class="col-12">
                   <div class="q-mb-sm">{{ $t('pages.public.register.fields.phoneNumber') }}</div>
-                  <q-input
-                    rounded outlined
-                    color="primary"
-                    name="phone_number"
-                    v-model="authStore.registerState.phone_number"
-                    type="text"
-                    :rules="[(val) => !!val || $t('general.fields.requiredStringField')]"
-                  />
+                  <q-input rounded outlined color="primary" name="phone_number"
+                    v-model="authStore.registerState.phone_number" type="text"
+                    :rules="[(val) => !!val || $t('general.fields.requiredStringField')]" />
                 </div>
                 <div class="col-12">
                   <div class="q-mb-sm">{{ $t('pages.public.register.fields.password') }}</div>
-                    <q-input
-                    rounded outlined
-                      color="primary"
-                      v-model="authStore.registerState.password"
-                      :type="showPass ? 'password' : 'text'"
-                      :rules="[(val) => !!val || $t('general.fields.requiredStringField')]"
-                    >
-                      <template v-slot:append>
-                        <q-icon
-                          :name="showPass ? 'visibility_off' : 'visibility'"
-                          class="cursor-pointer"
-                          @click="showPass = !showPass"
-                        />
-                      </template>
-                    </q-input>
-                  </div>
-                  <div class="col-12">
-                    <q-btn
-                      rounded
-                      color="primary"
-                      class="full-width q-pa-md"
-                      :label="$t('pages.public.register.register')"
-                      type="submit"
-                      :disable="
-                        // authStore.registerState.phone_country_code.length < 1 ||
-                        authStore.registerState.phone_number.length < 1 ||
-                        authStore.registerState.first_name.length < 1 ||
-                        authStore.registerState.last_name.length < 1 ||
-                        authStore.registerState.password.length < 1
-                      "
-                    />
-                  </div>
+                  <q-input rounded outlined color="primary" v-model="authStore.registerState.password"
+                    :type="showPass ? 'password' : 'text'"
+                    :rules="[(val) => !!val || $t('general.fields.requiredStringField')]">
+                    <template v-slot:append>
+                      <q-icon :name="showPass ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                        @click="showPass = !showPass" />
+                    </template>
+                  </q-input>
+                </div>
+                <div class="col-12">
+                  <q-btn rounded color="primary" class="full-width q-pa-md"
+                    :label="$t('pages.public.register.register')" type="submit" :disable="
+            // authStore.registerState.phone_country_code.length < 1 ||
+            authStore.registerState.phone_number.length < 1 ||
+            authStore.registerState.first_name.length < 1 ||
+            authStore.registerState.last_name.length < 1 ||
+            authStore.registerState.password.length < 1
+            " />
                 </div>
               </div>
+            </div>
           </form>
           <div class="flex row justify-center items-center q-mt-md">
             <p>
               {{ $t('pages.public.register.isUser') }}
-              <router-link class="text-primary text-bold text-body1" style="text-decoration: none;" :to="{ name: 'LoginPage' }">{{ $t('pages.public.register.loginHere') }}</router-link>
+              <router-link class="text-secondary text-bold text-body1" style="text-decoration: none;"
+                :to="{ name: 'LoginPage' }">{{ $t('pages.public.register.loginHere') }}</router-link>
             </p>
           </div>
         </div>
       </q-card-section>
-      <q-card-section class="bg-primary col-12 col-md-6 mobile-hide q-pa-xl flex items-center justify-center" style="border-radius: 35px;">
+      <q-card-section class="bg-primary col-12 col-md-6 mobile-hide q-pa-xl flex items-center justify-center"
+        style="border-radius: 35px;">
         <signup-image />
       </q-card-section>
     </q-card>
