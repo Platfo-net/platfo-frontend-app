c51d2457d2203fca2e116b4cb9513f37361d35e4
Hamid <samimi.electronics@gmail.com>
2024-05-02 21:27:01 +0330

    [feature] update ui

--- src/components/bot/TelegramBotItem.vue
+++ src/components/bot/TelegramBotItem.vue
@@ -22,7 +22,7 @@ const { mutateAsync: updateBotAsync, isPending: isUpdatingBot } = botService.tel
 const { data: chatbot, isLoading: chatbotIsLoading, isError: chatbotIsError, error: chatbotError } = botService.telegram.queries.getBotChatbot(props.bot.id);
 const { data: chatbotList, isLoading: chatbotListLoading } = chatbotService.queries.getChatbotList();
 const { mutateAsync: registerChatbotAsync } = botService.telegram.mutations.registerChatbot(props.bot.id);
-const { mutateAsync: deleteChatbotAsync } = botService.telegram.mutations.deleteChatbot(props.bot.id);
+const { mutateAsync: deleteChatbotAsync, isPending: chatbotIsDeleting } = botService.telegram.mutations.deleteChatbot(props.bot.id);
 const showRegisterChatbot = ref(false)
 const chatbotId = ref<string>();
 /****************** */
@@ -58,96 +58,70 @@ const handleSubmit = async () => {
             </q-form>
         </q-card>
     </q-dialog>
-    <q-form @submit.prevent="handleSubmit">
-        <q-card flat bordered class="bg-grey-12">
-            <q-card flat>
-                <q-card-section class="text-body1">
-                    اطلاعات بات
-                </q-card-section>
-                <q-card-section>
-                    <div class="row">
-                        <div class="q-mr-md">
-                            <q-img :src="updateModel.image_url" class="rounded-borders bg-grey-12" height="100px"
-                                width="100px" style="border: 1px solid #e2e2e2;"></q-img>
+
+    <q-card flat bordered class="bg-grey-12">
+        <q-card flat>
+            <q-card-section>
+                <div class="row">
+                    <div class="q-mr-md">
+                        <q-img :src="updateModel.image_url" class="rounded-borders bg-grey-12" height="100px"
+                            width="100px" style="border: 1px solid #e2e2e2;"></q-img>
+                    </div>
+                    <div>
+                        <div class="q-mb-sm">
+                            <div class="text-grey">
+                                آیدی بات
+                            </div>
+                            <div dir="ltr">@{{ bot.username }}</div>
                         </div>
                         <div>
-                            <div class="q-mb-sm">
-                                <div class="text-grey">
-                                    یوزرنیم
-                                </div>
-                                <div>{{ bot.username }}</div>
+                            <div class="text-grey">
+                                عنوان بات
                             </div>
-                            <div>
-                                <div class="text-grey">
-                                    نام
-                                </div>
-                                <div>{{ bot.first_name }}</div>
-                            </div>
+                            <div>{{ bot.first_name }}</div>
                         </div>
                     </div>
-                </q-card-section>
-                <q-card-section>
-                </q-card-section>
-            </q-card>
-            <q-card flat bordered>
-                <q-card-section class="text-body1">
-                    تنظیمات بات
-                </q-card-section>
-                <q-card-section>
-                    <q-input clearable class="bg-white" outlined label="پیام خوش آمد گویی" type="textarea"
-                        v-model="updateModel.welcome_message"></q-input>
-                </q-card-section>
-                <q-card-section>
-                    <q-input clearable class="bg-white" outlined label="عنوان دکمه" type="text"
-                        v-model="updateModel.button_name"></q-input>
-                </q-card-section>
-                <q-card-section>
-                    <q-input dir="ltr" clearable class="bg-white" outlined label="لینک اپلیکیشن" type="url"
-                        v-model="updateModel.app_link"></q-input>
-                </q-card-section>
-                <q-expansion-item default-opened header-class="text-body1" label="چت بات"
-                    style="border-top: 1px solid #e2e2e2; border-bottom: 1px solid #e2e2e2;">
-                    <q-linear-progress v-if="chatbotIsLoading" size="2px" indeterminate></q-linear-progress>
-                    <q-item v-if="chatbotIsError && chatbotError?.response.status === 404">
-                        <q-item-section>چت باتی ثبت نشده است</q-item-section>
-                        <q-item-section>
-                            <q-btn @click="showRegisterChatbot = true" color="secondary">ثبت چت بات</q-btn>
-                        </q-item-section>
-                    </q-item>
-                    <q-item v-else-if="chatbot">
-                        <q-list>
-                            <q-item>
-                                <q-item-section class="text-bold">نام</q-item-section>
-                                <q-item-section>{{ chatbot.name }}</q-item-section>
-                            </q-item>
-                            <q-item>
-                                <q-item-section class="text-bold">تمپرچر</q-item-section>
-                                <q-item-section>{{ chatbot.temperature }}</q-item-section>
-                            </q-item>
-                            <q-item>
-                                <q-btn round size="sm" icon='delete' rounded color="negative" @click="async () => {
-        await deleteChatbotAsync();
-        chatbot = undefined;
-    }"></q-btn>
-                                <q-item-section class="text-bold"></q-item-section>
-                            </q-item>
-                        </q-list>
-                    </q-item>
-                </q-expansion-item>
-                <!-- <q-card-section>
-                    <q-file clearable class="bg-white" outlined label="عکس بات" type="file" v-model="updateModel.image">
-                        <template v-slot:prepend>
-                            <q-icon name="attach_file" />
-                        </template>
-</q-file>
-</q-card-section> -->
-            </q-card>
-            <q-card-section class="row justify-end">
-                <q-btn type="submit" size="sm" :color="isFormChangedChanged() ? 'primary' : 'grey'"
-                    :disable="!isFormChangedChanged()" label="ذخیره تغییرات" :loading="isUpdatingBot"></q-btn>
+                </div>
             </q-card-section>
         </q-card>
-    </q-form>
+        <q-expansion-item header-style="border-top: 1px solid #e2e2e2" header-class="text-body1" label="تنظیمات بات"
+            class="bg-white">
+            <q-form @submit.prevent="handleSubmit">
+                <q-item>
+                    <q-input clearable class="bg-white full-width" outlined label="پیام خوش آمد گویی" type="textarea"
+                        v-model="updateModel.welcome_message"></q-input>
+                </q-item>
+                <q-item><q-input clearable class="bg-white full-width" outlined label="عنوان دکمه" type="text"
+                        v-model="updateModel.button_name"></q-input></q-item>
+                <q-item><q-input dir="ltr" clearable class="bg-white full-width" outlined label="لینک اپلیکیشن"
+                        type="url" v-model="updateModel.app_link"></q-input></q-item>
+                <q-item><q-btn class="q-mb-md full-width" type="submit"
+                        :color="isFormChangedChanged() ? 'primary' : 'grey'" :disable="!isFormChangedChanged()"
+                        label="ذخیره تغییرات" :loading="isUpdatingBot"></q-btn></q-item>
+
+            </q-form>
+        </q-expansion-item>
+        <q-expansion-item default-opened header-style="border-top: 1px solid #e2e2e2" header-class="text-body1"
+            label="اتصال چت بات" class="bg-white">
+            <q-linear-progress v-if="chatbotIsLoading" size="2px" indeterminate></q-linear-progress>
+            <div v-if="chatbotIsError && chatbotError?.response.status === 404"
+                class="flex row items-center justify-between q-pa-md">
+                <div>چت باتی ثبت نشده است</div>
+                <q-btn @click="showRegisterChatbot = true" color="secondary">ثبت چت بات</q-btn>
+            </div>
+            <div v-else-if="chatbot" class="flex row items-center justify-between q-pa-md">
+                <div class="flex column">
+                    <div class="text-grey-6 q-mb-sm">نام بات تلگرام</div>
+                    <div class="text-body2">{{ chatbot.name }}</div>
+                </div>
+                <q-btn color="negative" @click="async () => {
+        await deleteChatbotAsync();
+        chatbot = undefined;
+    }" round icon="delete" :loading="chatbotIsDeleting" flat>
+                </q-btn>
+            </div>
+        </q-expansion-item>
+    </q-card>
 </template>
 
 <style scoped></style>
