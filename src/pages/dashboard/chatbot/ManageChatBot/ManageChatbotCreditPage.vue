f59e1897c5777c6632dc3d1a5b1252e889b149aa
Hamid <samimi.electronics@gmail.com>
2024-05-05 22:53:32 +0330

    [feature] format code

--- src/pages/dashboard/chatbot/ManageChatBot/ManageChatbotCreditPage.vue
+++ src/pages/dashboard/chatbot/ManageChatBot/ManageChatbotCreditPage.vue
@@ -48,7 +48,8 @@ const { mutateAsync: buyPlan, isPending: butPlanIsPending } = chatbotService.cre
                         :label="showCreditDrawer ? 'بستن منو' : 'منو'" @click="showCreditDrawer = !showCreditDrawer" />
                 </q-toolbar>
             </q-header>
-            <q-drawer class="flex column justify-between q-pa-md" show-if-above v-model="showCreditDrawer" side="left" bordered>
+            <q-drawer class="flex column justify-between q-pa-md" show-if-above v-model="showCreditDrawer" side="left"
+                bordered>
                 <q-list dense>
                     <template v-for="section in creditSections" :key="section.id">
                         <q-expansion-item
@@ -64,7 +65,8 @@ const { mutateAsync: buyPlan, isPending: butPlanIsPending } = chatbotService.cre
                             </q-list>
                         </q-expansion-item>
                         <q-item class="q-ma-sm rounded-borders" @click="manageCreditSection = section.id"
-                                    :active="manageCreditSection === section.id" active-class="bg-blue-1 text-blue" clickable v-ripple v-else>
+                            :active="manageCreditSection === section.id" active-class="bg-blue-1 text-blue" clickable
+                            v-ripple v-else>
                             <q-item-section>{{ section.title }}</q-item-section>
                         </q-item>
                     </template>
@@ -78,30 +80,38 @@ const { mutateAsync: buyPlan, isPending: butPlanIsPending } = chatbotService.cre
                     <q-tab-panels v-model="manageCreditSection">
                         <q-tab-panel name="chatbot_credit_transactions">
                             <q-table flat bordered :columns="[
-                                { label: 'نام اشتراک', name: 'title', field: 'title', align: 'left' },
-                                { label: 'قابل پرداخت', name: 'amount', field: 'amount', align: 'left' },
-                                { label: 'تاریخ سند', name: 'created_at', field: 'amount', align: 'left' },
-                                { label: 'وضعیت پرداخت', name: 'is_paid', field: 'is_paid', align: 'left' },
-                                { label: 'اشتراک مازاد', name: 'is_extra', field: 'is_extra', align: 'left' },
-                                { label: 'تاریخ پرداخت', name: 'paid_at', field: 'paid_at', align: 'left' },
-                                { label: 'تعداد روز افزایش', name: 'extend_days', field: 'extend_days', align: 'left' },
-                                { label: 'تعداد چت', name: 'extend_chat_count', field: 'extend_chat_count', align: 'left' },
-                                { label: 'تعداد توکن', name: 'extend_token_count', field: 'extend_token_count', align: 'left' },
-                                { label: 'عملیات', name: 'actions', field: 'actions', align: 'left' },
-                            ]" :rows="chatbotCreditSubscriptionTransactions" :loading="chatbotCreditSubscriptionTransactionsIsLoading">
+                        { label: 'نام اشتراک', name: 'title', field: 'title', align: 'left' },
+                        { label: 'قابل پرداخت', name: 'amount', field: 'amount', align: 'left' },
+                        { label: 'تاریخ سند', name: 'created_at', field: 'amount', align: 'left' },
+                        { label: 'وضعیت پرداخت', name: 'is_paid', field: 'is_paid', align: 'left' },
+                        { label: 'اشتراک مازاد', name: 'is_extra', field: 'is_extra', align: 'left' },
+                        { label: 'تاریخ پرداخت', name: 'paid_at', field: 'paid_at', align: 'left' },
+                        { label: 'تعداد روز افزایش', name: 'extend_days', field: 'extend_days', align: 'left' },
+                        { label: 'تعداد چت', name: 'extend_chat_count', field: 'extend_chat_count', align: 'left' },
+                        { label: 'تعداد توکن', name: 'extend_token_count', field: 'extend_token_count', align: 'left' },
+                        { label: 'عملیات', name: 'actions', field: 'actions', align: 'left' },
+                    ]" :rows="chatbotCreditSubscriptionTransactions"
+                                :loading="chatbotCreditSubscriptionTransactionsIsLoading">
                                 <template v-slot:body-cell-is_paid="props">
                                     <q-td>
-                                        <q-badge :label="props.row.is_paid ? 'پرداخت شده' : 'پرداخت نشده'" :color="props.row.is_paid ? 'teal-1' : 'red-1'" :class="props.row.is_paid ? 'text-teal' : 'text-red'"></q-badge>
+                                        <q-badge :label="props.row.is_paid ? 'پرداخت شده' : 'پرداخت نشده'"
+                                            :color="props.row.is_paid ? 'teal-1' : 'red-1'"
+                                            :class="props.row.is_paid ? 'text-teal' : 'text-red'"></q-badge>
                                     </q-td>
                                 </template>
                                 <template v-slot:body-cell-is_extra="props">
                                     <q-td>
-                                        <q-badge :label="props.row.is_extra ? 'بله' : 'خیر'" :color="props.row.is_extra ? 'teal-1' : 'red-1'" :class="props.row.is_extra ? 'text-teal' : 'text-red'"></q-badge>
+                                        <q-badge :label="props.row.is_extra ? 'بله' : 'خیر'"
+                                            :color="props.row.is_extra ? 'teal-1' : 'red-1'"
+                                            :class="props.row.is_extra ? 'text-teal' : 'text-red'"></q-badge>
                                     </q-td>
                                 </template>
                                 <template v-slot:body-cell-actions="props">
                                     <q-td>
-                                        <q-btn :label="!props.row.is_paid ? 'پرداخت' : 'پرداخت شده'" :color="props.row.is_paid ? 'grey' : 'blue'" size="sm" :disable="props.row.is_paid"></q-btn>
+                                        <q-btn :label="!props.row.is_paid ? 'پرداخت' : 'پرداخت شده'"
+                                            :color="props.row.is_paid ? 'grey-1' : 'blue-1'"
+                                            :text-color="props.row.is_paid ? 'grey' : 'blue'" size="sm" unelevated
+                                            :disable="props.row.is_paid"></q-btn>
                                     </q-td>
                                 </template>
                             </q-table>
@@ -111,12 +121,14 @@ const { mutateAsync: buyPlan, isPending: butPlanIsPending } = chatbotService.cre
                                 <q-linear-progress indeterminate size="2px" color="primary"></q-linear-progress>
                             </template>
                             <template v-else>
-                                <div v-if="!chatbotCreditSubscriptionPlans || chatbotCreditSubscriptionPlans.length < 1">
+                                <div
+                                    v-if="!chatbotCreditSubscriptionPlans || chatbotCreditSubscriptionPlans.length < 1">
                                     پلنی برای نمایش وجود ندارد...
                                 </div>
-                                <div v-else  class="row q-col-gutter-md" >
-                                    <div v-for="plan in chatbotCreditSubscriptionPlans.filter(x => x.is_active)" :key="plan.uuid" class="col-12 col-md-4">
-                                        <q-card flat bordered class="bg-grey-1" >
+                                <div v-else class="row q-col-gutter-md">
+                                    <div v-for="plan in chatbotCreditSubscriptionPlans.filter(x => x.is_active)"
+                                        :key="plan.uuid" class="col-12 col-md-4">
+                                        <q-card flat bordered class="bg-grey-1">
                                             <q-card-section class="text-h5">
                                                 اشتراک {{ plan.title }}
                                             </q-card-section>
@@ -129,32 +141,40 @@ const { mutateAsync: buyPlan, isPending: butPlanIsPending } = chatbotService.cre
                                                     رایگان!
                                                 </div>
                                                 <div v-else class="text-h6">
-                                                    {{plan.original_price}} <TomanSymbol :size="24"></TomanSymbol> / در ماه
+                                                    {{ plan.original_price }} <TomanSymbol :size="24"></TomanSymbol> /
+                                                    در
+                                                    ماه
                                                 </div>
                                             </q-card-section>
                                             <q-card-section style="border-top: 1px solid #e2e2e2;">
                                                 <q-list>
                                                     <q-item>
-                                                        <q-item-section side><q-icon name="done"></q-icon></q-item-section>
+                                                        <q-item-section side><q-icon
+                                                                name="done"></q-icon></q-item-section>
                                                         <q-item-section>{{ plan.extend_days }} روزه</q-item-section>
                                                     </q-item>
                                                     <q-item>
-                                                        <q-item-section side><q-icon name="done"></q-icon></q-item-section>
-                                                        <q-item-section>{{ plan.extend_token_count }} توکن</q-item-section>
+                                                        <q-item-section side><q-icon
+                                                                name="done"></q-icon></q-item-section>
+                                                        <q-item-section>{{ plan.extend_token_count }}
+                                                            توکن</q-item-section>
                                                     </q-item>
                                                     <q-item>
-                                                        <q-item-section side><q-icon name="done"></q-icon></q-item-section>
+                                                        <q-item-section side><q-icon
+                                                                name="done"></q-icon></q-item-section>
                                                         <q-item-section>{{ plan.extend_chat_count }} چت</q-item-section>
                                                     </q-item>
                                                 </q-list>
                                             </q-card-section>
                                             <q-card-section style="border-top: 1px solid #e2e2e2;">
-                                                <q-btn class="full-width" unelevated :label="plan.original_price ? 'خرید اشتراک' : 'فعال سازی اشتراک'" color="teal-1" text-color="teal" @click="async () => {
-                                                    await buyPlan({
-                                                        planId: plan.uuid,
-                                                    });
-                                                    manageCreditSection = 'chatbot_credit_transactions';
-                                                }" :loading="butPlanIsPending"></q-btn>
+                                                <q-btn class="full-width" unelevated
+                                                    :label="plan.original_price ? 'خرید اشتراک' : 'فعال سازی اشتراک'"
+                                                    color="teal-1" text-color="teal" @click="async () => {
+                        await buyPlan({
+                            planId: plan.uuid,
+                        });
+                        manageCreditSection = 'chatbot_credit_transactions';
+                    }" :loading="butPlanIsPending"></q-btn>
                                             </q-card-section>
                                         </q-card>
                                     </div>
