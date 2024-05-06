c4bdead8fd0ce6ddf9d9121b4ae62b8e72b4dd11
Hamid <samimi.electronics@gmail.com>
2024-05-06 11:38:07 +0330

    [feature] default openned if inner route is current route

--- src/components/common/BaseLayoutSidebarMenu.vue
+++ src/components/common/BaseLayoutSidebarMenu.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { QIcon } from 'quasar';
 import { SidebarRoute } from 'src/composables/use-sidebar';
+import { useRoute } from 'vue-router';
 
 /** COMPONENT COMPOSABLES */
-
+const route = useRoute();
 /************************ */
 
 /** COMPONENT STATE */
@@ -31,18 +32,19 @@ defineProps<BaseLayoutSidebarMenuProps>();
 <template>
   <q-scroll-area style="height: calc(100% - 161.6px); margin-top: 161.6px">
     <q-list padding>
-      <div v-for="(route, idx) in routes" :key="route.title + idx">
-        <q-item :active-class="route.activeClass?.length ? route.activeClass : 'bg-grey-3'
-        " clickable v-if="!route.children?.length" :to="route.to">
+      <div v-for="(_route, idx) in routes" :key="_route.title + idx">
+        <q-item :active-class="_route.activeClass?.length ? _route.activeClass : 'bg-grey-3'
+        " clickable v-if="!_route.children?.length" :to="_route.to">
           <q-item-section avatar>
-            <q-icon :name="route.icon"></q-icon>
+            <q-icon :name="_route.icon"></q-icon>
           </q-item-section>
           <q-item-section>
-            {{ route.title }}
+            {{ _route.title }}
           </q-item-section>
         </q-item>
-        <q-expansion-item v-else :label="route.title" :icon="route.icon">
-          <div v-for="(innerRoute, innerIdx) in route.children" :key="innerRoute.title + innerIdx">
+        <q-expansion-item :default-opened="_route.children.map(x => x.to?.name).includes(route.name!)" v-else
+          :label="_route.title" :icon="_route.icon">
+          <div v-for="(innerRoute, innerIdx) in _route.children" :key="innerRoute.title + innerIdx">
             <q-item :active-class="innerRoute.activeClass?.length
         ? innerRoute.activeClass
         : 'bg-grey-3'
