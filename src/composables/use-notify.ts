6acb336f39acdc28cd7d3786bc0bdeecd77af006
Hamid <samimi.electronics@gmail.com>
2024-05-13 01:10:24 +0330

    [feature] change notification styles

--- src/composables/use-notify.ts
+++ src/composables/use-notify.ts
@@ -1,42 +1,21 @@
 import { Notify } from 'quasar';
 
 export function useNotify() {
-  function success(
-    msg?: string,
-    caption?: string,
-    time?: number,
-    icon?: string
-  ) {
+  function success(message?: string, icon?: string) {
     return Notify.create({
-      message: !msg
-        ? ' '
-        : `<div style="font-size:17px;font-weight:500;">${msg}</div>`,
-      icon: !icon ? 'check_circle' : icon,
-      caption: !caption
-        ? ' '
-        : `<div style="font-size:14px;font-weight:500;">${caption}</div>`,
-      textColor: 'positive',
-      color: 'success-color-100',
-      classes: 'success',
-      timeout: !time ? 3000 : time,
-      html: true,
+      color: 'teal-1',
+      message,
+      textColor: 'teal',
+      icon,
     });
   }
 
-  function error(msg?: string, caption?: string, time?: number, icon?: string) {
+  function error(message?: string, icon?: string) {
     return Notify.create({
-      message: !msg
-        ? ' '
-        : `<div style="font-size:17px;font-weight:500;">${msg}</div>`,
-      icon: !icon ? 'error' : icon,
-      caption: !caption
-        ? ' '
-        : `<div style="font-size:14px;font-weight:500;">${caption}</div>`,
-      textColor: 'negative',
-      color: 'danger-color-100',
-      classes: 'danger',
-      timeout: !time ? 3000 : time,
-      html: true,
+      message,
+      icon,
+      textColor: 'red',
+      color: 'red-1',
     });
   }
 
