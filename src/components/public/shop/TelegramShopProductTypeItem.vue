b75bb077341eb0d6d08cb665dcf3d1e148ef3aee
Arsalan-mehranfar <mohammadmehranfar1999@gmail.com>
2024-05-17 01:44:45 +0430

    [debug] delete shop line

--- src/components/public/shop/TelegramShopProductTypeItem.vue
+++ src/components/public/shop/TelegramShopProductTypeItem.vue
@@ -39,19 +39,45 @@ const shopCart = useShoppingCart();
   </div>
   <div class="flex row">
     <template v-if="shopCart.getItemCount(product) > 0">
-      <div class="flex row q-pa-sm justify-between items-center"
-        style="border: 1px solid #e2e2e2; border-radius: 4px; box-shadow: #e2e2e2 0px 0px 15px; width: 143px;">
-        <q-btn @click="shopCart.add(product)" class="q-mx-sm" icon="add" color="white" text-color="grey-8"
-          size="sm" dense flat></q-btn>
+      <div
+        class="flex row q-pa-sm justify-between items-center"
+        style="
+          border: 1px solid #e2e2e2;
+          border-radius: 4px;
+          box-shadow: #e2e2e2 0px 0px 15px;
+          width: 143px;
+        "
+      >
+        <q-btn
+          @click="shopCart.add(product)"
+          class="q-mx-sm"
+          icon="add"
+          color="white"
+          text-color="grey-8"
+          size="sm"
+          dense
+          flat
+        ></q-btn>
         <small class="q-mx-sm">{{ shopCart.getItemCount(product) }}</small>
-        <q-btn @click="shopCart.remove(product)" class="q-mx-sm"
+        <q-btn
+          @click="shopCart.remove(product)"
+          class="q-mx-sm"
           :icon="shopCart.getItemCount(product) === 1 ? 'delete' : 'remove'"
-          :text-color="shopCart.getItemCount(product) === 1 ? 'red-8' : 'dark'" size="sm" dense flat></q-btn>
+          :text-color="shopCart.getItemCount(product) === 1 ? 'red-8' : 'dark'"
+          size="sm"
+          dense
+          flat
+        ></q-btn>
       </div>
     </template>
     <template v-else>
       <div class="flex row">
-        <q-btn color="dark" label="افزودن" @click="shopCart.add(product)" style="width: 143px;"></q-btn>
+        <q-btn
+          color="dark"
+          label="افزودن"
+          @click="shopCart.add(product)"
+          style="width: 143px"
+        ></q-btn>
       </div>
     </template>
   </div>
