c45355b3d749851d036ea673fb593ba6e410b3bf
Hamid <samimi.electronics@gmail.com>
2024-06-13 02:33:57 +0330

    feat: change create shop to dialog refactor: use service instead of data fetch logic in component

--- src/components/dashboard/store-page/StoreList.vue
+++ src/components/dashboard/store-page/StoreList.vue
@@ -1,51 +1,30 @@
 <script setup lang="ts">
-import { useApi } from 'src/composables/use-api';
-const { api, loading } = useApi();
 import StoreListItem from 'components/dashboard/store-page/StoreListItem.vue';
-import { IShop } from 'components/models';
-import { ref } from 'vue';
 import BaseLoadingSpinner from 'components/common/BaseLoadingSpinner.vue';
 import { useShopService } from 'src/services/useShopService';
 
 const shopService = useShopService();
 
+const { data: shopList, isPending: shopListIsPending } = shopService.queries.getShopList();
 const { data: shopCategories, isPending: isCategoriesLoading } = shopService.queries.getShopCategories();
 
-const getShops = async () => {
-  try {
-    const { data } = await api.get<IShop[]>('/shop/shop/all');
-    return data;
-  } catch (err) {
-    console.error(err);
-    return [];
-  }
-};
-const shops = ref<IShop[]>([]);
-
-getShops().then((data) => {
-  shops.value = data;
-});
 </script>
 
 <template>
   <div class="row q-col-gutter-md">
-    <template v-if="loading"
-      ><base-loading-spinner loading></base-loading-spinner
-    ></template>
-    <template v-else-if="shops.length < 1">
+    <template v-if="shopListIsPending">
+      <base-loading-spinner loading></base-loading-spinner>
+    </template>
+    <template v-else-if="!shopList || shopList.length < 1">
       <p>
         {{
-          $t('pages.panel.dashboard.storeListPage.messages.noShopsRegistered')
-        }}
+      $t('pages.panel.dashboard.storeListPage.messages.noShopsRegistered')
+    }}
       </p>
     </template>
-    <template v-else v-for="(shop, idx) in shops" :key="`${idx}-${shop.id}`">
-      <StoreListItem
-        :category="(shopCategories?.find(x => x.value == shop.category)?.title as string)"
-        :description="shop.description"
-        :id="shop.id"
-        :title="shop.title"
-      />
+    <template v-else v-for="(shop, idx) in shopList || []" :key="`${idx}-${shop.id}`">
+      <StoreListItem :category="(shopCategories?.find(x => x.value == shop.category)?.title as string)"
+        :description="shop.description" :id="shop.id" :title="shop.title" />
     </template>
   </div>
 </template>
--- src/pages/dashboard/store/StoreListPage.vue
+++ src/pages/dashboard/store/StoreListPage.vue
@@ -1,21 +1,82 @@
 <script setup lang="ts">
 import StoreList from 'components/dashboard/store-page/StoreList.vue';
 import LayoutBreadCrumbs from 'components/BreadCrumbs.vue';
+import { ref } from 'vue';
+import { CreateShopDto } from 'src/types';
+import { useShopService } from 'src/services/useShopService';
+import { useNotify } from 'src/composables/use-notify';
+const notify = useNotify();
+const shopService = useShopService();
+const { mutateAsync: createShopAsync, isPending: createShopIsPending } = shopService.mutations.createShop();
+const { data: shopCategories, isPending: isCategoriesLoading } = shopService.queries.getShopCategories();
+const showCreateDialog = ref(false);
+const createShopModel = ref<CreateShopDto>({
+  title: '',
+  description: '',
+  category: '',
+  color_code: ''
+});
+const handleFormSubmit = async () => {
+  try {
+    await createShopAsync(createShopModel.value);
+    showCreateDialog.value = false;
+    notify.success('ایجاد فروشگاه موفقیت آمیز بود', 'check');
+  } catch (err) {
+    notify.error('ایجاد فروشگاه با خطا مواجه شد', 'error');
+  }
+}
+const clearCreateShopModel = () => createShopModel.value = {
+  title: '',
+  description: '',
+  category: '',
+  color_code: ''
+};
 </script>
 
 <template>
   <q-page class="q-pa-md container">
     <LayoutBreadCrumbs />
-
+    <q-dialog @hide="clearCreateShopModel" v-model="showCreateDialog">
+      <q-card style="min-width: 367px">
+        <q-card-section>
+          <div class="text-h6">چت بات جدید</div>
+        </q-card-section>
+        <q-card-section>
+          <q-form @submit.prevent="handleFormSubmit">
+            <q-input class="q-mb-md" outlined label="عنوان *" type="text" v-model="createShopModel.title" lazy-rules
+              :rules="[
+      (val) =>
+        (val && val.length > 0) ||
+        $t('general.fields.requiredStringField'),
+    ]"></q-input>
+            <q-select v-model="createShopModel.category" outlined label="دسته بندی" :loading="isCategoriesLoading"
+              lazy-rules :options="shopCategories?.map(({ title, value }) => ({ label: title, value }))" emit-value
+              map-options :rules="[(val) => (val && val.length > 0) || 'لطفاً گزینه ای را انتخاب کنید']">
+            </q-select>
+            <q-input class="q-mb-md" outlined type="textarea" v-model="createShopModel.description" dense label="شرح"
+              lazy-rules></q-input>
+            <q-input label="تم رنگی فروشگاه" outlined dir="ltr" v-model="createShopModel.color_code"
+              class="my-input q-mb-md">
+              <template v-slot:append>
+                <q-icon name="colorize" class="cursor-pointer">
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-color v-model="createShopModel.color_code" default-value="#ff9800" no-header no-footer />
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+            <q-btn unelevated type="submit" class="q-mb-md full-width" color="teal-1" text-color="teal" label="ایجاد"
+              :loading="createShopIsPending"></q-btn>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
     <div class="row justify-between items-center q-mb-lg">
       <div class="text-h5">
         {{ $t('pages.panel.dashboard.storeListPage.title') }}
       </div>
-      <q-btn
-        color="dark"
-        :to="{ name: 'CreateStorePage' }"
-        :label="$t('pages.panel.dashboard.storeListPage.createNewStore')"
-      ></q-btn>
+      <q-btn @click="showCreateDialog = true" color="dark"
+        :label="$t('pages.panel.dashboard.storeListPage.createNewStore')"></q-btn>
     </div>
     <StoreList />
   </q-page>
